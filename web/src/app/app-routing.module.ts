b86df687a1fe49a9e64536d3f5a7428e50fae2b2
henrick <henrickzero@gmail.com>
2025-09-04 22:36:15 -0300

    ajustes

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component'; 
 import { SignupComponent } from './components/signup/signup.component'; 
 import { SankeyDataComponent } from './components/sankey-app/sankey-data.component'; 
+import { SindicatoComponent } from './components/sindicato/sindicato.component';
 
 
 const routes: Routes = [
@@ -15,6 +16,7 @@ const routes: Routes = [
   { path: 'sankey', component: SankeyAppComponent, canActivate: [AuthGuard] },
     { path: 'editsankey', component: SankeyDataComponent, canActivate: [AuthGuard] },
   { path: 'matriz', component: MatrizComponent, canActivate: [AuthGuard] },
+  { path: 'sindicato/:id', component: SindicatoComponent },
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
   { path: '**', component: HomeComponent, canActivate: [AuthGuard] }
